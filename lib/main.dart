75fd5a4f637620dc4fc1d18a7ed4dbe3816bcbb2
ilhuitem0c <jc11111001001@gmail.com>
2022-08-28 23:33:19 -0500

    Calculating a total score

--- lib/main.dart
+++ lib/main.dart
@@ -21,19 +21,33 @@ class _MyAppState extends State<MyApp>{
   final _questions = const [
       {'questionText': '¿Cuál es tu comida favorita?', 
       'answers': [
-        'Chilaquiles', 'Tacos', 'Gorditas', 'Pizza'
+        {'text': 'Chilaquiles', 'score': 5}, 
+        {'text': 'Tacos', 'score': 15}, 
+        {'text': 'Gorditas', 'score': 5}, 
+        {'text': 'Pizza', 'score': 10}
       ]},
-      {'questionText': '¿Cuál es tu mascota favorita?', 'answers': [
-        'Newton', 'Lisa', 'Sumi', 'Babona'
+      {'questionText': '¿Cuál es tu mascota favorita?', 
+      'answers': [
+        {'text': 'Newton', 'score': 15}, 
+        {'text': 'Lisa', 'score' : 10}, 
+        {'text': 'Sumi', 'score': 15}, 
+        {'text': 'Babona', 'score': 0}
       ]},
-      {'questionText': '¿Cuál es tu libro favorito?', 'answers': [
-        'Los Miserables', 'El señor de los anillos', 'Anna Kerenina', 'Los hermanos Karamazov'
+      {'questionText': '¿Cuál es tu libro favorito?', 
+      'answers': [
+        {'text': 'Los Miserables', 'score': 15}, 
+        {'text': 'El señor de los anillos', 'score': 10}, 
+        {'text': 'Anna Kerenina', 'score': 10}, 
+        {'text': 'Los hermanos Karamazov', 'score': 5}
       ]},
     ]; 
 
-  var _questionIndex = 0; 
+  var _questionIndex = 0;
+  var _totalScore = 0;  
 
-  void _answerQuestion(){
+  void _answerQuestion(int score){
+
+    _totalScore += score; 
 
     setState((){
       _questionIndex = _questionIndex + 1; 
